4156f4b325e552b24f1026a9f7e2f56c0882c100
Isaac-Isaac <Isaac.Isaac@gmail.com>
2023-09-17 19:52:22 -0500

    main

---
+++ main.js
@@ -0,0 +1,66 @@
+function handle_file(file) {
+    console.log(file)
+    document.getElementById("file_hover_popup").style.display = "none";
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.addEventListener("dragover", (ev) => {
+    document.getElementById("file_hover_popup").style.display = "block";
+    ev.preventDefault();
+}, false);
+
+window.addEventListener("dragleave",(ev) => {
+    document.getElementById("file_hover_popup").style.display = "none";
+    ev.preventDefault();
+},false);
+
+window.addEventListener("drop",(ev) => {
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+
+    if (ev.dataTransfer.items) {
+        // Use DataTransferItemList interface to access the file(s)
+        [...ev.dataTransfer.items].forEach((item, i) => {
+            // If dropped items aren't files, reject them
+            if (item.kind === "file") {
+                const file = item.getAsFile();
+                handle_file(file);
+            }
+        });
+    } else {
+        // Use DataTransfer interface to access the file(s)
+        [...ev.dataTransfer.files].forEach((file, i) => {
+            handle_file(file);
+        });
+    }
+},false)
+
+document.getElementById("input_main_upload").addEventListener("change", ()=> {
+    files = document.getElementById("input_main_upload").files;
+    if (!!files[0]) {
+        handle_file(files[0]);
+    }
+})
