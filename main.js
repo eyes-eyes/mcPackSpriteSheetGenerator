0967ffbb20646f5b0ac924b90edca442d69ac323
Isaac-Isaac <Isaac.Isaac@gmail.com>
2023-09-22 18:21:09 -0500

    main

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
-const body = document.querySelector('body');
+/*const body = document.querySelector('body');
 const upload = document.querySelector('.upload');
 const uploadButtonText = document.querySelector*('.upload-button-text');
 const uploadFilename = document.querySelector('.upload-filename');
-const fileInput = document.getElementById*('file');
-const dropArea = document.querySelector('.drop-area');
+const fileInput = document.getElementById('file');
+const dropArea = document.querySelector('.drop-area');*/
+
+let file_object_raw = null;
 
 function handle_file(file) {
     console.log(file)
     document.getElementById("file_hover_popup").style.display = "none";
+    document.getElementById("main_file_selector_areas").style.display = "none";
+    document.getElementById("main_site_data").style.display = "block";
+    file_object_raw = file;
+    
 } 
 
+function back_to_file_selector() {
+    document.getElementById("main_file_selector_areas").style.display = "block";
+    document.getElementById("main_site_data").style.display = "none";
+}
 
 
 
@@ -32,42 +42,47 @@ function handle_file(file) {
 
 
 
-
-
-window.addEventListener("dragover", (ev) => {
-    document.getElementById("file_hover_popup").style.display = "block";
-    ev.preventDefault();
-}, false);
-
-window.addEventListener("dragleave",(ev) => {
-    document.getElementById("file_hover_popup").style.display = "none";
-    ev.preventDefault();
-},false);
-
-window.addEventListener("drop",(ev) => {
-    // Prevent default behavior (Prevent file from being opened)
-    ev.preventDefault();
-
-    if (ev.dataTransfer.items) {
-        // Use DataTransferItemList interface to access the file(s)
-        [...ev.dataTransfer.items].forEach((item, i) => {
-            // If dropped items aren't files, reject them
-            if (item.kind === "file") {
-                const file = item.getAsFile();
+function install_event_listners() {
+    window.addEventListener("dragover", (ev) => {
+        document.getElementById("file_hover_popup").style.display = "block";
+        ev.preventDefault();
+    }, false);
+    
+    window.addEventListener("dragleave",(ev) => {
+        document.getElementById("file_hover_popup").style.display = "none";
+        ev.preventDefault();
+    },false);
+    
+    window.addEventListener("drop",(ev) => {
+        // Prevent default behavior (Prevent file from being opened)
+        ev.preventDefault();
+    
+        if (ev.dataTransfer.items) {
+            // Use DataTransferItemList interface to access the file(s)
+            [...ev.dataTransfer.items].forEach((item, i) => {
+                // If dropped items aren't files, reject them
+                if (item.kind === "file") {
+                    const file = item.getAsFile();
+                    handle_file(file);
+                }
+            });
+        } else {
+            // Use DataTransfer interface to access the file(s)
+            [...ev.dataTransfer.files].forEach((file, i) => {
                 handle_file(file);
-            }
-        });
-    } else {
-        // Use DataTransfer interface to access the file(s)
-        [...ev.dataTransfer.files].forEach((file, i) => {
-            handle_file(file);
-        });
-    }
-},false)
+            });
+        }
+    },false)
+    
+    document.getElementById("input_main_upload").addEventListener("change", ()=> {
+        files = document.getElementById("input_main_upload").files;
+        if (!!files[0]) {
+            handle_file(files[0]);
+        }
 
-document.getElementById("input_main_upload").addEventListener("change", ()=> {
-    files = document.getElementById("input_main_upload").files;
-    if (!!files[0]) {
-        handle_file(files[0]);
-    }
-})
+        document.getElementById("input_main_upload").value = "";
+    })
+}
+
+
+window.addEventListener("load",install_event_listners);
