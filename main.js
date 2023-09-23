a73063441c7cf6b20933e0f15e5e5ffaaca93c9e
Isaac-Isaac <Isaac.Isaac@gmail.com>
2023-09-23 01:45:46 -0500

    main

--- main.js
+++ main.js
@@ -7,13 +7,54 @@ const dropArea = document.querySelector('.drop-area');*/
 
 let file_object_raw = null;
 let file_object_zip = null;
-let zip_path_objects = {}
+let zip_path_objects = {};
+let pack_mcmetta_data = {};
 
 
+
+//Constants
+const version_codes = {
+    "1": ["1.6.1", "1.8.9"],
+    "2": ["1.9", "1.10.2"],
+    "3": ["1.11", "1.12.2"],
+    "4": ["1.13", "1.14.4"],
+    "5": ["1.15", "1.16.1"],
+    "6": ["1.16.2", "1.16.5"],
+    "7": ["1.17", "1.17.1"],
+    "8": ["1.18", "1.18.2"],
+    "9": ["1.19", "1.19.2"],
+    "10": ["1.19", "1.19.2"],
+    "11": ["1.19.2", "1.19.2"],
+    "12": ["1.19.3"],
+    "13": ["1.19.4"],
+    "14": ["1.19.4"],
+    "15": ["1.20", "1.20.1"],
+    "16": ["1.20", "1.20.1"],
+    "17": ["1.20", "1.20.1"],
+    "18": ["1.20.2"]
+}
+
+
+//Constants
+
+
+
+let fileNameElement = document.querySelector("#main_site_data p");
 
 function zip_new_entry_handler(entries) {
-    entries.
-    zip_path_objects
+    for (i in entries) {
+        path_arrary = entries[i].filename.split("/")
+
+        current_path_loc = zip_path_objects
+        for (o in path_arrary) {
+            current_path_loc = current_path_loc[path_arrary[o]] ? current_path_loc[path_arrary[o]] : {}
+        }
+        current_path_loc = entries[i]
+    }
+
+    
+
+    // pack_data_parse()
 }
 
 function handle_file(file) {
@@ -23,10 +64,11 @@ function handle_file(file) {
     document.getElementById("main_site_data").style.display = "block";
     file_object_raw = file;
 
+    fileNameElement.innerText = file.name;
+
     file_object_zip = new zip.ZipReader(new zip.BlobReader(file));
     file_object_zip.getEntries().then(zip_new_entry_handler)
 
-    
 } 
 
 function back_to_file_selector() {
